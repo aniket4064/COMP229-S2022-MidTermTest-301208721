4fbc84ee3e8bfaa69431e287c4c58ad4003adfb4
TonnieLamar <80836487+TonnieLamar@users.noreply.github.com>
2022-06-25 23:33:31 +0300

    Initial commit

---
+++ server/config/db.js
@@ -0,0 +1,12 @@
+/*
+Name : Sungmin Lee
+Student # : 301136725
+Course Code : COMP229
+Course Name : Web application Development
+Mid-Term Test
+*/
+module.exports = {
+  //local MongoDB deployment ->
+  //"URI": "mongodb://localhost/books229"
+  "URI": "mongodb+srv://AniketMehta:kTcwVdvFfRtuKrX2@mongodbserver.hvluc.mongodb.net/Books?retryWrites=true&w=majority"
+};
